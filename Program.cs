71807354e8abf65a0382afe30a1b645a8c86aae0
Prasanna Shan <sporigins@gmail.com>
2020-07-04 11:55:37 -0400

    SOLID refactoring in group breakouts

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidRefactoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
